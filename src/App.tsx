ee26668f026e3a8f6d8519e3b4cdbfc70e4a587c
anx450z <anx450z@gmail.com>
2022-08-03 12:13:55 +0530

    add static pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/pages/Layout';
+import Homepage from './components/pages/Homepage';
+import Contact from './components/pages/Contact';
 
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path= "/" element={<Layout />}>  
+        <Route index element={<Homepage />}/>
+        <Route path = "contact" element={<Contact />}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
