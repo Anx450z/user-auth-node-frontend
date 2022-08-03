adc6fb2284dfe78eb15ef3b0b2564a13de0bd0ca
anx450z <anx450z@gmail.com>
2022-08-03 15:15:13 +0530

    add image and Tabs

--- src/components/pages/LogiReg.tsx
+++ src/components/pages/LogiReg.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import { Grid, Card, Tabs, Tab, Box } from "@mui/material";
+import React, { useState } from "react";
+
+const shopping_image = require("../../images/shopping_image.png");
+
+const TabPanel = (props: any) => {
+  const { children, value, index } = props;
+  return (
+    <div role="tabpanel" hidden={value !== index}>
+      {value === index && <Box>{children}</Box>}
+    </div>
+  );
+};
 
 const LogiReg = () => {
+  const [value, setValue] = useState(0);
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
+
   return (
     <>
-    <h1>Login/registration</h1>
+      <Grid container sx={{ height: "90vh" }}>
+        <Grid
+          item
+          lg={7}
+          sm={5}
+          sx={{
+            backgroundImage: `url(${shopping_image})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}></Grid>
+        <Grid item lg={5} sm={7}>
+          <Card sx={{ width: "100%", height: "100%" }}>
+            <Box>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                  value={value as number}
+                  textColor="secondary"
+                  indicatorColor="secondary"
+                  onChange={handleChange}>
+                  <Tab label="Login" sx={{ fontWeight: 700 }}></Tab>
+                  <Tab label="Registration" sx={{ fontWeight: 700 }}></Tab>
+                </Tabs>
+              </Box>
+              <TabPanel value={value} index={0}>
+                User Login
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                User Registration
+              </TabPanel>
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
     </>
-  )
-}
+  );
+};
 
-export default LogiReg
+export default LogiReg;
