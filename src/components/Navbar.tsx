e4d2b80e726264a0cfdc8acda77fc74b8f1fecda
anx450z <anx450z@gmail.com>
2022-08-05 01:51:11 +0530

    add conditional Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { getToken } from "../services/LocalStorageService";
 
 const Navbar = () => {
+  const token = getToken();
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
@@ -17,9 +19,16 @@ const Navbar = () => {
             <Button component={NavLink} to="/contact" sx={{ color: "white" }}>
               Contact
             </Button>
-            <Button component={NavLink} to="/login" sx={{ color: "white" }}>
-              Login
-            </Button>
+            {token ? (
+              <Button
+                component={NavLink}
+                to="/profile"
+                sx={{ color: "white" }}>profile</Button>
+            ) : (
+              <Button component={NavLink} to="/login" sx={{ color: "white" }}>
+                Login
+              </Button>
+            )}
           </Toolbar>
         </AppBar>
       </Box>
